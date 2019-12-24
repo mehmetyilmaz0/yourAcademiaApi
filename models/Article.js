da1f4538afc1fdabb57e0e105872ad32f49ad5b1
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-12-24 17:20:21 +0300

    validation of article and Category

--- models/Article.js
+++ models/Article.js
@@ -4,36 +4,46 @@ const Schema = mongoose.Schema;
 const ArticleSchema = new Schema({
     title: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` Alanı Zorunludur...'],
+        minLength: [1, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Küçük Olamaz '],
+        maxLength: [100, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Büyük Olamaz '],
     },
     content: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` Alanı Zorunludur...'],
+        minLength: [1, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Küçük Olamaz '],
+        maxLength: [100, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Büyük Olamaz '],
     },
     categoryId: {
         type: Schema.Types.ObjectId,
     },
     keywords: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` Alanı Zorunludur...'],
+        minLength: [1, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Küçük Olamaz '],
+        maxLength: [100, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Büyük Olamaz '],
     },
     favCount: {
         type: Number,
-        required: true,
+        required: [true, '`{PATH}` Alanı Zorunludur...'],
         default: 0
     },
     displayCount: {
         type: Number,
-        required: true,
+        required: [true, '`{PATH}` Alanı Zorunludur...'],
         default: 0
     },
     source: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` Alanı Zorunludur...'],
+        minLength: [1, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Küçük Olamaz '],
+        maxLength: [300, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Büyük Olamaz '],
     },
     user: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` Alanı Zorunludur...'],
+        minLength: [1, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Küçük Olamaz '],
+        maxLength: [100, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Büyük Olamaz '],
     },
     date: {
         type: Date,
--- models/Category.js
+++ models/Category.js
@@ -4,11 +4,15 @@ const Schema = mongoose.Schema;
 const CategorySchema = new Schema({
     title: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` Alanı Zorunludur...'],
+        minLength: [1, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Küçük Olamaz '],
+        maxLength: [100, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Büyük Olamaz '],
     },
     content: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` Alanı Zorunludur...'],
+        minLength: [1, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Küçük Olamaz '],
+        maxLength: [100, '`{PATH}` Alanının Uzunluğu `{VALUE}` den Büyük Olamaz '],
     },
     date: {
         type: Date,
