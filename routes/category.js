82365ef0aaf934ed1d6881f1afa99dc07bfb059e
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-12-24 15:58:11 +0300

    post method and get All for category

--- routes/category.js
+++ routes/category.js
@@ -5,7 +5,31 @@ const router = express.Router();
 const Category = require('../models/Category');
 
 router.get('/', (req, res, next) => {
-    res.json({ title: 'Express' });
+    const promise = Category.find({});
+
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+
+});
+
+router.post('/', (req, res, next) => {
+    const {title, content} = req.body;
+
+    const category = new Category({
+        title: title,
+        content: content
+    });
+
+    const promise = category.save();
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    })
+
 });
 
 module.exports = router;
