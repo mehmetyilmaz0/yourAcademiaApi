fedf06fae258f9a75deda0b0a3d066e835a10dc7
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-12-24 16:35:52 +0300

    find method for one category by id

--- routes/category.js
+++ routes/category.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -6,6 +7,57 @@ const Category = require('../models/Category');
 
 router.get('/', (req, res, next) => {
     const promise = Category.aggregate([
+        {
+            $lookup: {
+                from:'articles',
+                localField: '_id',
+                foreignField: 'categoryId',
+                as: 'articles'
+            }
+        },
+        {
+            $unwind: {
+                path: '$articles',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {
+            $group: {
+                _id: {
+                    _id: '$_id',
+                    title: '$title',
+                    content: '$content'
+                },
+                articles: {
+                    $push: '$articles'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: '$_id._id',
+                title: '$_id.title',
+                content: '$_id.content',
+                articles: '$articles'
+            }
+        }
+    ]);
+
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+
+});
+
+router.get('/:categoryId', (req, res, next) => {
+    const promise = Category.aggregate([
+        {
+          $match: {
+              _id: mongoose.Types.ObjectId(req.params.categoryId)
+          }
+        },
         {
             $lookup: {
                 from:'articles',
