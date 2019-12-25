f4b1641a51ecf1d462852b9fd61ecbae739b345f
mehmetpdf <mehmet.yilmaz2@outlook.com.tr>
2019-12-25 13:53:00 +0300

    test - post a article test

--- test/api/article-test.js
+++ test/api/article-test.js
@@ -9,10 +9,11 @@ let token;
 chai.use(chaiHttp);
 
 describe('/api/articles test', () => {
+
     before((done) => {
         chai.request(server)
             .post('/authenticate')
-            .send( {username: 'mehmet.yilmaz2', password: 'Sanane1234'})
+            .send( {username: 'mehmet.yilmaz', password: 'Sanane1234'})
             .end((err, res) => {
                 token = res.body.token;
                 console.log('token', token);
@@ -31,5 +32,38 @@ describe('/api/articles test', () => {
                     done();
                 });
         });
+    });
+
+    describe('/POST articles', () => {
+       it('(POST/ARTICLE) Article Kaydeder', (done) => {
+           const article = {
+               title: 'Title_Mocha',
+               content: 'Content_Mocha',
+               categoryId: '5e020dcb796c631ca0bae02d',
+               keywords: 'Keywords_Mocha1,Keywords_Mocha2',
+               favCount: 0,
+               displayCount: 0,
+               source: 'www.sourceMocha.com',
+               user: 'admin_mocha'
+           };
+
+           chai.request(server)
+               .post('/api/articles')
+               .send(article)
+               .set('x-access-token', token)
+               .end((err, res) =>{
+                   res.should.have.status(200);
+                   res.body.should.be.a('object');
+                   res.body.should.have.property('title');
+                   res.body.should.have.property('content');
+                   res.body.should.have.property('categoryId');
+                   res.body.should.have.property('keywords');
+                   res.body.should.have.property('favCount');
+                   res.body.should.have.property('displayCount');
+                   res.body.should.have.property('source');
+                   res.body.should.have.property('user');
+                   done();
+               } );
+       });
     });
 });
